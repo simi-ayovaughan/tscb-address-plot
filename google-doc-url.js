912991b2ccbc498d1c397058702a5432b8e77256
Simi A <122497678+simi-ayovaughan@users.noreply.github.com>
2023-01-12 11:18:22 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ftqM4mk3ZyJcIk3JNUgRCgoN6QH5T3dc7Cu_dhXP_W8/edit#gid=1080875718';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
